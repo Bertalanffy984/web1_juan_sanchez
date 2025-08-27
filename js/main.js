062d819548019b4e42d906209b9f66925f171c27
Bertalanffy984 <szjuanda@outlook.com>
2025-08-27 06:16:22 -0500

    version_2

--- js/main.js
+++ js/main.js
@@ -3,5 +3,25 @@ window.onscroll=function(){
     var media=window.matchMedia("(min-width: 768px)").matches;
     media? contain_head.classList.toggle('cambiar-contain-barra',window.scrollY > 935):
     contain_head.classList.toggle('cambiar-contain-barra',window.scrollY > 600)
-
 }
+
+
+// (function () {
+//     const list = document.querySelector('.contents');
+//     const menu = document.querySelector('.menu');
+//     menu.addEventListener('click', () => {
+//     list.classList.toggle('viw-tabla');
+//     });
+
+// })();
+document.addEventListener('DOMContentLoaded', function () {
+    var media=window.matchMedia("(min-width: 768px)").matches;
+    if(!media){
+    const list = document.querySelector('.contents');
+    const menu = document.querySelector('.menu');
+    menu.addEventListener('click', () => {
+    list.classList.toggle('viw-tabla');
+    });
+}
+});
+
